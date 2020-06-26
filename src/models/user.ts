ca60aa705a5933c47c67019614e10edcd026947b
Shya <rumarenko3@gmail.com>
2020-06-27 02:55:06 +0300

    get user's uncompleted surveys endpoint

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,20 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, {Schema, Document, Types} from "mongoose";
 import express, {Response} from "express"
+
 
 export interface UserInterface extends Document{
   _id: string;
   username: string;
   password: string;
   role: string;
+  completedSurveys: mongoose.Types.ObjectId[]
 }
 
 const UserSchema: Schema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
+  completedSurvey: {type: Array(mongoose.Types.ObjectId), required: true},
 });
 
 const User = mongoose.model<UserInterface>("User", UserSchema);
