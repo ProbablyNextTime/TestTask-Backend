eddc01dcba04d8aa9b72614f8f412c923e1c3f5f
Shya <rumarenko3@gmail.com>
2020-06-28 16:49:17 +0300

    fixes: many-to-many

--- src/models/survey.ts
+++ src/models/survey.ts
@@ -2,12 +2,14 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface SurveyInterface extends Document{
   questions: string[],
-  tittle: string,
+  title: string,
+  users: mongoose.Types.ObjectId[]
 }
 
 const SurveySchema: Schema = new Schema({
   questions: { type: Array(String), required: true },
-  tittle: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  users: { type: Array(mongoose.Types.ObjectId), required: true},
 });
 
 const Survey = mongoose.model<SurveyInterface>("Survey", SurveySchema);
