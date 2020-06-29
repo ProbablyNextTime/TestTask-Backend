1efcbcd2ae3406d238e6068a1251c28e4807b382
Shya <rumarenko3@gmail.com>
2020-06-29 14:19:43 +0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/surveys", surveysRouter);
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://compassionate-varahamihira-ae291d.netlify.app');
   res.setHeader('Access-Control-Allow-Credentials', "true");
   res.setHeader('Access-Control-Allow-Methods', ['PATCH', 'POST', 'GET', 'DELETE', 'PUT']);
   res.setHeader('Access-Control-Allow-Headers', ['Content-Type']);
