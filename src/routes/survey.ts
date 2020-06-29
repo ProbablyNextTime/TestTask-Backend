dc2457171e81c9d6273f7439de30d643a860eb5f
Shya <rumarenko3@gmail.com>
2020-06-29 17:15:15 +0300

    deploy

--- src/routes/survey.ts
+++ src/routes/survey.ts
@@ -68,7 +68,9 @@ surveysRouter.get("/", async (req: Request, res: Response) => {
 surveysRouter.get("/:Id", async (req: Request, res: Response) => {
   try {
       // Get surveys from DB
-      const survey: SurveyInterface | null = await Survey.findOne({_id: req.query.Id})
+      console.log(req.query.Id);
+      console.log(req.params.Id);
+      const survey: SurveyInterface | null = await Survey.findOne({_id: req.params.Id})
       if(survey) {
         res.status(200).send({survey: survey})
       } else {
